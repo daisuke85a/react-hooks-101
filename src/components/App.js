3b78958dba37bf541972842a03d8529ea7ac8566
daisuke <dsaito85a@gmail.com>
2021-02-10 13:19:50 +0900

    create operationLogs reducer

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ import reducer from '../reducers/index'
 
 const App = () => {
   const initailState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initailState)
   return (
